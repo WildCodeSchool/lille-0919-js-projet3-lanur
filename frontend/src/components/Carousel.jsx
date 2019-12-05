4ff730262595dfdcbcd04d5a31632ca0600c92fb
Adrien Imberdis <AdrienImberdis@gmail.com>
2019-12-05 12:06:59 +0100

    Creation of the components folder with a style folder, creation of 2 components: Carousel.jsx and Carousel.scss, using react-animated-slider module for the carousel

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,52 @@
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import React from "react";
+import "./style/Carousel.scss";
+
+export default function Carousel() {
+  const content = [
+    {
+      title: "John C.",
+      role: " Pro-Player",
+      description:
+        "Lorem ipsum fezokfoefpokmofkslkfpkpefkpkskkdpoazdkpoagtafdytfdgd_yèfdygyfuèe",
+      image:
+        "http://www.influencia.net/data/classes/actualite/actu_6082_image2012rectangle_petite.png"
+    },
+    {
+      title: "Harry D.",
+      role: "Coach",
+      description:
+        "Lorem ipsum fzeopzfkpzkefpoekfefzftydyazftydafauzdyfazytdfuyqfzdyufqzfdyu",
+      image:
+        "http://getwallpapers.com/wallpaper/full/2/0/2/764295-black-purple-background-3200x1800-for-meizu.jpg"
+    },
+    {
+      title: "Samuel N.",
+      role: "Player",
+      description:
+        "lorem ipsum tgee zerfz fz fzjn e o z zefzrf z z egfzgef grfed grfedz tgfrde tgrfde gtrf",
+      image:
+        "http://getwallpapers.com/wallpaper/full/2/0/2/764295-black-purple-background-3200x1800-for-meizu.jpg"
+    }
+  ];
+  return (
+    <Slider autoplay={3000} className="slider">
+      {content.map((item, index) => (
+        <div
+          key={index}
+          style={{
+            background: `url('${item.image}') no-repeat center`,
+            backgroundSize: "cover"
+          }}
+        >
+          <div className="center">
+            <h1>{item.title}</h1>
+            <h2>{item.role}</h2>
+            <p>{item.description}</p>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  );
+}
