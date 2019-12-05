75321b2da83134fcc3ae12a547bf3479a7649696
Gumatan <arnaut.demaret@gmail.com>
2019-12-05 11:31:07 +0100

    created SignIn.jsx component w/ basic form and uniformised base css

---
+++ frontend/src/components/SignIn.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const SignIn = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+  return (
+    <div className="SignIn">
+      <h2>
+        <span>Vous avez</span> un compte ?
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <h3>Identifiant</h3>
+        <input type="text" />
+        <h3>Mot de passe</h3>
+        <input type="text" />
+        <p>mot de passe oublié ?</p>
+        <button type="submit" value="Envoyer">
+          Se connecter
+        </button>
+      </form>
+      <h3>
+        Vous n'avez <span>PAS</span> de compte ?
+      </h3>
+      <div>
+        <button>Créer un compte</button>
+        <p>Continuer sans créer de compte</p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
