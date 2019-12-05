926bb6ac3147c770dda8d941733a8c2151b7a8b2
jervalles <jer.vallles@outlook.com>
2019-12-05 17:01:56 +0100

    DropDown menu added under Evenement on the navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,39 +1,86 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style/NavBar.scss";
 
-function NavBar() {
-  return (
-    <div className="main-NavBar">
-      <h1>
-        <a href="#">
-          <span className="Lan">LAN'</span>U.R
-        </a>
-      </h1>
-      <ul>
-        <li>
+class NavBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: true
+    };
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.closeMenu);
+    });
+  }
+
+  closeMenu(event) {
+    if (!this.dropdownMenu.contains(event.target)) {
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener("click", this.closeMenu);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="main-NavBar">
+        <h1>
           <a href="#">
-            <span className="FirstLetter">A</span>ctus
+            <span className="Lan">LAN'</span>U.R
           </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="FirstLetter">T</span>eams
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="FirstLetter">É</span>vènements
-            <img className="triangle" src="../images/Black_triangle.svg"></img>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="FirstLetter">P</span>rofil
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+        </h1>
+        <ul>
+          <li>
+            <a href="#">
+              <span className="FirstLetter">A</span>ctus
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="FirstLetter">T</span>eams
+            </a>
+          </li>
+          <li onClick={this.showMenu}>
+            <a href="#">
+              <span className="FirstLetter">É</span>vènements
+              <img
+                className="triangle"
+                src="../images/Black_triangle.svg"
+              ></img>
+            </a>
+            {this.state.showMenu ? (
+              <div
+                className="dropDownMenu"
+                ref={element => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <li>
+                  <span className="FirstLetter">A</span>genda
+                </li>
+                <li>
+                  <span className="FirstLetter">R</span>ésultats
+                </li>
+              </div>
+            ) : null}
+          </li>
+
+          <li>
+            <a href="#">
+              <span className="FirstLetter">P</span>rofil
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
