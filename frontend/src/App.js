ef529f89271a300ff149f2d1c347c2f1cc4229e9
Gautier Mille <gautier.mille@gmail.com>
2019-12-05 12:14:17 +0100

    adjsutment on the grey bacground

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import NewsFeed from "./components/NewsFeed";
 import NavBar from "./components/NavBar";
 import "./components/style/Reset.css";
+import "./App.css";
+
 import Postcard from "./components/Postcard";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <NewsFeed />
+      {/* <NewsFeed /> */}
       <Postcard />
       <Postcard />
       <Postcard />
