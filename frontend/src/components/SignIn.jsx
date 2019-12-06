ba1bf922783c0c1dbeaa4d98099740dc0d4a65c2
Gumatan <arnaut.demaret@gmail.com>
2019-12-06 09:16:14 +0100

    added style to SignIn component and added buttons to reset rules

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -7,25 +7,35 @@ const SignIn = () => {
   };
   return (
     <div className="SignIn">
-      <h2>
-        <span>Vous avez</span> un compte ?
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <h3>Identifiant</h3>
-        <input type="text" />
-        <h3>Mot de passe</h3>
-        <input type="text" />
-        <p>mot de passe oublié ?</p>
-        <button type="submit" value="Envoyer">
-          Se connecter
-        </button>
-      </form>
-      <h3>
-        Vous n'avez <span>PAS</span> de compte ?
-      </h3>
-      <div>
-        <button>Créer un compte</button>
-        <p>Continuer sans créer de compte</p>
+      <div className="catchPhrase">
+        <h1>
+          <span>LAN'</span>U.R
+        </h1>
+        <h2>
+          Vivons l'<span>E-Sport</span> ensemble
+        </h2>
+      </div>
+      <div className="connect">
+        <h2>
+          <span>Vous avez</span> un compte ?
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <h3>Identifiant</h3>
+          <input type="text" />
+          <h3>Mot de passe</h3>
+          <input type="text" />
+          <p>mot de passe oublié ?</p>
+          <button type="submit" value="Envoyer">
+            Se connecter
+          </button>
+        </form>
+        <h3>
+          Vous n'avez <span>PAS</span> de compte ?
+        </h3>
+        <div className="noAccount">
+          <button>Créer un compte</button>
+          <p>Continuer sans créer de compte</p>
+        </div>
       </div>
     </div>
   );
