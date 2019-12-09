e9346f567f8f8cae7007d91836c5e74de9d3a38e
jervalles <jer.vallles@outlook.com>
2019-12-09 09:25:14 +0100

    removed useless reset.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import NewsFeed from "./components/NewsFeed";
 import NavBar from "./components/NavBar";
 import "./components/style/Reset.css";
 import "./App.css";
-import LiveContainer from "./components/LiveContainer";
-
-
-import Postcard from "./components/Postcard";
 
 function App() {
   return (
