3b44d66d4aefe87f7a6feb35c4f25f1efd938a4a
Gautier Mille <gautier.mille@gmail.com>
2019-12-11 13:38:43 +0100

    delete component import from react on a function component

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style/NewsFeed.scss";
 import Postcard from "./Postcard";
 import LiveContainer from "./LiveContainer";
