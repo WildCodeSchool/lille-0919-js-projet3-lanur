0c0b1a49e029d4003508cabdf3ff92855c7be6fe
jervalles <jer.vallles@outlook.com>
2019-12-12 10:11:36 +0100

    added some comments

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.get("/", (req, res) => {
   res.send("Bienvenue sur Express");
 });
 
+// USERS || GET & POST
 app.get("/api/users", (req, res) => {
   db.query(
     "SELECT id, firstname, lastname, team_id, pseudo, sexe, address, avatar, email, age, country, city, twitch, bio, mixer, youtube, LOL_pseudo, Fortnite_pseudo, CSGO_pseudo, OW_pseudo, HOTS_pseudo, SMITE_pseudo, APEX_pseudo, STARCRAFT2_pseudo, Hearstone_pseudo, KROSMAGA_pseudo, SSBU_pseudo, Tekken_pseudo, SF5_pseudo, ROCKETLEAGUE_pseudo, TFT_pseudo, PUBG_pseudo, R6S_pseudo, Paladins_pseudo from user",
@@ -52,6 +53,7 @@ app.post("/api/newuser", (req, res) => {
   });
 });
 
+// FIL-ACTU || GET & POST
 app.get("/api/postslist", (req, res) => {
   db.query("SELECT * from post", (err, results) => {
     if (err) {
