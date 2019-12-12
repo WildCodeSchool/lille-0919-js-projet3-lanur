459c2130141318e665fe3b61ad68856e359a179a
jervalles <jer.vallles@outlook.com>
2019-12-12 10:43:46 +0100

    correction on the 200 status

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,7 @@ app.get("/api/postslist", (req, res) => {
 
 app.post("/api/newpost", (req, res) => {
   const formData = req.body;
-  db.query("INSERT INTO post SET ?", formData, (err, results) => {
+  db.query("INSERT INTO post SET ?", formData, (err, res) => {
     if (err) {
       console.log(err);
       res.status(500).send("Erreur lors de la sauvegarde du message");
