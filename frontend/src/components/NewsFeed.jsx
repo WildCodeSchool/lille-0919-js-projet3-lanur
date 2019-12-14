653418eb5794e310899aefc8bdf3d65990a42126
jervalles <jer.vallles@outlook.com>
2019-12-14 11:58:42 +0100

    Postfield component created with a sample Form and added into NewsFeed

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./style/NewsFeed.scss";
 import Postcard from "./Postcard";
 import LiveContainer from "./LiveContainer";
+import PostField from "./PostField";
 
 function NewsFeed() {
   return (
     <div className="main-NewsFeed">
+      <PostField />
       <Postcard />
       <Postcard />
       <Postcard />
