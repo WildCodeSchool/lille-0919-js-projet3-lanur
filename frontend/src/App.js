49e0b985b59cbe7f0244a48e06ec37d51e1ba0c9
jervalles <jer.vallles@outlook.com>
2019-12-14 12:00:30 +0100

    needed to showup the NavBar and NewsFeed components to see the render. so i re added them into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./components/style/reset.css";
 import "./App.css";
-
-
+import NewsFeed from "./components/NewsFeed";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
+      <NewsFeed />
     </div>
   );
 }
