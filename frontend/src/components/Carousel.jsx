feefcdd208c5dd860a61db57416b1bb779721120
nassilio <hubmattfac@fac.com>
2019-12-16 10:46:12 +0100

    added specific svg arrows to slider instead of default arrows

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -39,7 +39,7 @@ export default function Carousel() {
     }
   ];
   return (
-    <Slider autoplay={5000} className="slider">
+    <Slider autoplay={5000} className="slider" previousButton nextButton>
       {content.map((item, index) => (
         <div
           key={index}
