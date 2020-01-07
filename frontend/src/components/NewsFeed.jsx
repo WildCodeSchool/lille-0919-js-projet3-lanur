d9ba7d7e78c6ba15e3aada830d8bbbf111a8ca7c
jervalles <jer.vallles@outlook.com>
2020-01-07 17:40:18 +0100

    changer class to function using Hooks and loading posts with axios from the backend

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./style/NewsFeed.scss";
 import Postcard from "./Postcard";
 import LiveContainer from "./LiveContainer";
+import PostField from "./PostField";
 
 function NewsFeed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/posts").then(({ data }) => {
+      setPosts(data);
+    });
+  });
+
   return (
     <div className="main-NewsFeed">
-      <Postcard />
-      <Postcard />
-      <Postcard />
-      <Postcard />
+      <PostField />
+      {posts.map(post => (
+        <Postcard message={post.message} date={post.date} />
+      ))}
       <LiveContainer />
     </div>
   );
