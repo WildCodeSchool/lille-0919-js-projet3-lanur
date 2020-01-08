d7f037f405f0b9a778adbfb911829024e6d78e61
Gautier Mille <gautier.mille@gmail.com>
2020-01-08 17:47:31 +0100

    dleete useles div

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -20,49 +20,40 @@ const SignIn = () => {
   return (
     <div>
       {!divCreation ? (
-        <ReactCSSTransitionGroup
-          transitionName="connectiontransition"
-          transitionAppear={false}
-          transitionDisappear={true}
-          transitionAppearTimeout={1500}
-          transitionEnter={true}
-          transitionLeave={true}
-        >
-          <div className="SignIn">
-            <div className="catchPhrase">
-              <h1>
-                <span>LAN'</span>U.R
-              </h1>
-              <h2>
-                Vivons l'<span>E-Sport</span> ensemble
-              </h2>
-            </div>
-            <div className="connect">
-              <h2>
-                <span>Vous avez</span> un compte ?
-              </h2>
-              <form onSubmit={handleSubmit}>
-                <h3>Identifiant</h3>
-                <input type="text" />
-                <h3>Mot de passe</h3>
-                <input type="password" />
-                <p>mot de passe oublié ?</p>
-                <button type="submit" value="Envoyer">
-                  Se connecter
-                </button>
-              </form>
-              <h3>
-                Vous n'avez <span>PAS</span> de compte ?
-              </h3>
-              <div className="noAccount">
-                <button onClick={accountCreation} className="accountCreation">
-                  Créer un compte
-                </button>
-                <p>Continuer sans créer de compte</p>
-              </div>
+        <div className="SignIn">
+          <div className="catchPhrase">
+            <h1>
+              <span>LAN'</span>U.R
+            </h1>
+            <h2>
+              Vivons l'<span>E-Sport</span> ensemble
+            </h2>
+          </div>
+          <div className="connect">
+            <h2>
+              <span>Vous avez</span> un compte ?
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <h3>Identifiant</h3>
+              <input type="text" />
+              <h3>Mot de passe</h3>
+              <input type="password" />
+              <p>mot de passe oublié ?</p>
+              <button type="submit" value="Envoyer">
+                Se connecter
+              </button>
+            </form>
+            <h3>
+              Vous n'avez <span>PAS</span> de compte ?
+            </h3>
+            <div className="noAccount">
+              <button onClick={accountCreation} className="accountCreation">
+                Créer un compte
+              </button>
+              <p>Continuer sans créer de compte</p>
             </div>
           </div>
-        </ReactCSSTransitionGroup>
+        </div>
       ) : (
         <div className="SignIn">
           <Form />
