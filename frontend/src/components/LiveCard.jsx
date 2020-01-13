6f8b57ba273da8eaa579d2351d7a287c90f4e3cc
Jeremy VALLES <jer.valles@outlook.com>
2020-01-13 15:53:34 +0100

    added a component LiveCard and added it in LiveContainer

---
+++ frontend/src/components/LiveCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style/LiveCard.scss";
+
+function LiveCard(props) {
+  return (
+    <div className="streamingStatus">
+      <img src="https://steamuserimages-a.akamaihd.net/ugc/2438013375545542318/66E1CB5FBE2D04901F697975545A88D6B412192C/" />
+      <img src="https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c540.png" />
+      Online - Name
+    </div>
+  );
+}
+
+export default LiveCard;
