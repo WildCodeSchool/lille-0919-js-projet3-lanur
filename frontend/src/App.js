cae26e4fad899e891dbaccb98413d5d42b6daa3e
Adrien Imberdis <AdrienImberdis@gmail.com>
2020-01-13 17:25:45 +0100

    app changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import ContainerEditProfile from "./components/EditProfile/ContainerEditProfile";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Carousel from "./components/Carousel";
+import SignIn from "./components/SignIn";
+import Form from "./components/Form";
+import NewsFeed from "./components/NewsFeed";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <ContainerEditProfile />
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Carousel} />
+        <Route exact path="/Signin" component={SignIn} />
+        <Route path="/Signin/Form" component={Form} />
+        <Route path="/NewsFeed" component={NewsFeed} />
+      </Switch>
     </div>
   );
-};
+}
 
 export default App;
