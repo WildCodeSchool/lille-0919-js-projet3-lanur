6fc1d0a8122b3ac290988ea6322f198a33592991
Jeremy VALLES <jer.valles@outlook.com>
2020-01-14 11:09:53 +0100

    added a slice to reduce the stream name

--- frontend/src/components/LiveCard.jsx
+++ frontend/src/components/LiveCard.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./style/LiveCard.scss";
 
 function LiveCard(props) {
-  console.log(props);
+  const streamTitle = props.stream_title;
   return (
     <div className="streamingStatus">
       <img src="https://steamuserimages-a.akamaihd.net/ugc/2438013375545542318/66E1CB5FBE2D04901F697975545A88D6B412192C/" />
       <img src="https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c540.png" />
-      {props.streamer_name} - {props.stream_title}
+      {props.streamer_name} -{" "}
+      {streamTitle.length > 20 ? streamTitle.slice(0, 16) + "..." : streamTitle}
     </div>
   );
 }
