03c8ff844be2607e0b28847fdd3b1d8de56b59de
Jeremy VALLES <jer.valles@outlook.com>
2020-01-16 20:30:19 +0100

    added an axios to havin logo depending of the game

--- frontend/src/components/LiveCard.jsx
+++ frontend/src/components/LiveCard.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "./Modal.jsx";
+import axios from "axios";
+import { backend } from "../conf.js";
 import useModal from "./useModal.js";
 import "./style/LiveCard.scss";
 
 function LiveCard(props) {
   const { isShowing, toggle } = useModal();
   const streamTitle = props.stream_title;
+  const [gameId, setGameId] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${backend}/api/gamelist/${props.game_id}`).then(({ data }) => {
+      let tmpGames = gameId;
+      const results = data;
+      tmpGames.push(...results);
+      setGameId(results);
+    });
+  }, []);
 
   return (
     <div className="streamingStatus" onClick={toggle}>
       <Modal isShowing={isShowing} hide={toggle} user_name={props.user_name} />
-
-      <img src={`/pictures/${props.game_id}.png`} />
+      {gameId.length > 0 ? (
+        <img
+          className="blank_game_logo"
+          src={`/games_icons/${gameId[0].id}.jpg`}
+        />
+      ) : (
+        <div className="blank_game_logo"></div>
+      )}
       <img src="https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c540.png" />
       <div className="streamCountainer">
         <div>
