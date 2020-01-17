a3e89e023102e99411ef366f497cbd3f0e50c562
Jeremy VALLES <jer.valles@outlook.com>
2020-01-17 18:10:12 +0100

    Addesd a missing game logo and the top streaming is now filtered by only eSport games

--- frontend/src/components/LiveContainer.jsx
+++ frontend/src/components/LiveContainer.jsx
@@ -10,7 +10,7 @@ function LiveContainer() {
   useEffect(() => {
     axios
       .get(
-        `https://api.twitch.tv/helix/streams?first=30&language=en&language=fr`,
+        `https://api.twitch.tv/helix/streams?first=100&language=en&language=fr`,
         {
           headers: {
             "Client-ID": twitch_Client_ID
@@ -21,7 +21,7 @@ function LiveContainer() {
         setLives(data.data);
       });
   }, []);
-
+  console.log(lives);
   return (
     <div className="allLive">
       <div className="liveTitle">
@@ -29,15 +29,36 @@ function LiveContainer() {
       </div>
       <div className="liveContent">
         {lives.length > 1
-          ? lives.map(live => (
-              <LiveCard
-                streamer_name={live.user_name}
-                stream_title={live.title}
-                viewer_count={live.viewer_count}
-                user_name={live.user_name}
-                game_id={live.game_id}
-              />
-            ))
+          ? lives
+              .filter(
+                ({ game_id }) =>
+                  game_id === "21779" ||
+                  game_id === "29595" ||
+                  game_id === "32959" ||
+                  game_id === "32507" ||
+                  game_id === "33214" ||
+                  game_id === "493057" ||
+                  game_id === "511224" ||
+                  game_id === "488552" ||
+                  game_id === "460630" ||
+                  game_id === "491115" ||
+                  game_id === "513143" ||
+                  game_id === "490422" ||
+                  game_id === "138585" ||
+                  game_id === "504461" ||
+                  game_id === "461067" ||
+                  game_id === "488615" ||
+                  game_id === "32399"
+              )
+              .map(live => (
+                <LiveCard
+                  streamer_name={live.user_name}
+                  stream_title={live.title}
+                  viewer_count={live.viewer_count}
+                  user_name={live.user_name}
+                  game_id={live.game_id}
+                />
+              ))
           : null}
       </div>
     </div>
