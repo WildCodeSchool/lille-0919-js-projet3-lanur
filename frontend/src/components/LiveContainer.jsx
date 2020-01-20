d13b9493bc82e4f1b6edd95f4f85cfca30e9ce9c
Jeremy VALLES <jer.valles@outlook.com>
2020-01-20 10:34:26 +0100

    .Filter is now directly in the useEffect

--- frontend/src/components/LiveContainer.jsx
+++ frontend/src/components/LiveContainer.jsx
@@ -18,7 +18,28 @@ function LiveContainer() {
         }
       )
       .then(({ data }) => {
-        setLives(data.data);
+        setLives(
+          data.data.filter(
+            ({ game_id }) =>
+              game_id === "21779" ||
+              game_id === "29595" ||
+              game_id === "32959" ||
+              game_id === "32507" ||
+              game_id === "33214" ||
+              game_id === "493057" ||
+              game_id === "511224" ||
+              game_id === "488552" ||
+              game_id === "460630" ||
+              game_id === "491115" ||
+              game_id === "513143" ||
+              game_id === "490422" ||
+              game_id === "138585" ||
+              game_id === "504461" ||
+              game_id === "461067" ||
+              game_id === "488615" ||
+              game_id === "32399"
+          )
+        );
       });
   }, []);
   return (
@@ -28,36 +49,15 @@ function LiveContainer() {
       </div>
       <div className="liveContent">
         {lives.length > 1
-          ? lives
-              .filter(
-                ({ game_id }) =>
-                  game_id === "21779" ||
-                  game_id === "29595" ||
-                  game_id === "32959" ||
-                  game_id === "32507" ||
-                  game_id === "33214" ||
-                  game_id === "493057" ||
-                  game_id === "511224" ||
-                  game_id === "488552" ||
-                  game_id === "460630" ||
-                  game_id === "491115" ||
-                  game_id === "513143" ||
-                  game_id === "490422" ||
-                  game_id === "138585" ||
-                  game_id === "504461" ||
-                  game_id === "461067" ||
-                  game_id === "488615" ||
-                  game_id === "32399"
-              )
-              .map((live) => (
-                <LiveCard
-                  streamer_name={live.user_name}
-                  stream_title={live.title}
-                  viewer_count={live.viewer_count}
-                  user_name={live.user_name}
-                  game_id={live.game_id}
-                />
-              ))
+          ? lives.map((live) => (
+              <LiveCard
+                streamer_name={live.user_name}
+                stream_title={live.title}
+                viewer_count={live.viewer_count}
+                user_name={live.user_name}
+                game_id={live.game_id}
+              />
+            ))
           : null}
       </div>
     </div>
