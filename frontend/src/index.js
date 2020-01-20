337877348bb5d561d7be2272f63190ec0b093ae8
Jeremy VALLES <jer.valles@outlook.com>
2020-01-20 10:30:04 +0100

    index.css was removed form the index.js, he is now back

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducer";
+import "./index.css";
 import "./reset.css";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
