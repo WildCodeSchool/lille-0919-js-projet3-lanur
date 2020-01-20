decaf13305147f5b99d3d6facb8b86270de8a528
nassilio <hubmattfac@fac.com>
2020-01-20 16:12:34 +0100

    linked Tag to the reducer

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import ReactTagInput from "@pathofdev/react-tag-input";
 import "@pathofdev/react-tag-input/build/index.css";
 import "./style/Tag.scss";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from "react-redux";
 
 function Tag() {
-  const [tags, setTags] = React.useState([]);
+  const [tags, setTags] = useState([]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (tags) {
+      dispatch({ type: "TAG", value: tags });
+    }
+  }, [tags]);
+  //const dispatch2 = () => useDispatch({ action: "TAG", value: tags });
+
   const notify = () => toast("Ne pas utiliser d'espace dans les tags.");
   return (
     <div className="tag">
--- frontend/src/store/reducer.js
+++ frontend/src/store/reducer.js
@@ -1,27 +1,31 @@
 const initialState = {
-    offsetPosts: 0,
-    jwt: null,
-    reload: 0
+  offsetPosts: 0,
+  jwt: null,
+  reload: 0,
+  tags: []
 };
 
 const reducer = (state = initialState, action) => {
-    const newState = { ...state }
-    switch (action.type) {
-        case 'PLUS_TEN':
-            newState.offsetPosts = newState.offsetPosts + 10
-            return newState
-        case 'RESET':
-            newState.offsetPosts = 0
-            newState.reload = newState.reload + 1
-            return newState
-        case "SAVE_JWT":
-            return {
-                ...state,
-                jwt: action.value
-            };
-        default:
-            return newState
-    }
-}
+  const newState = { ...state };
+  switch (action.type) {
+    case "PLUS_TEN":
+      newState.offsetPosts = newState.offsetPosts + 10;
+      return newState;
+    case "RESET":
+      newState.offsetPosts = 0;
+      newState.reload = newState.reload + 1;
+      return newState;
+    case "SAVE_JWT":
+      return {
+        ...state,
+        jwt: action.value
+      };
+    case "TAG":
+      newState.tags = action.value;
+      return newState;
+    default:
+      return newState;
+  }
+};
 
 export default reducer;
