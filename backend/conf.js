923969a7241d934b8f8498ca1d377ea8f4eb4f1d
Adrien Imberdis <AdrienImberdis@gmail.com>
2020-01-21 09:43:16 +0100

    putting the reducer in the store with the authentification système JWT

--- backend/conf.js
+++ backend/conf.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 
 const mysql = require("mysql");
-const backendPort = process.env.BACKEND_PORT || "4200";
+
+let CONFIG = {
+  backendPort: process.env.BACKEND_PORT || "4200",
+  jwtSecret: process.env.JWT_SECRET || "jwt_please_change",
+  saltRounds: process.env.SALT_ROUNDS || "10",
+  jwtExpiration: process.env.JWT_EXPIRATION || "10000"
+};
+
 const db = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST || "example.org", // adresse du serveur
@@ -17,9 +24,8 @@ cloudinary.config({
   api_secret: process.env.API_SECRET || "api_secret"
 });
 
-
 module.exports = {
-  backendPort,
+  CONFIG,
   db,
   cloudinary
 };
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { backendPort, db, cloudinary } = require("./conf.js");
 const multer = require("multer");
+const passport = require("passport");
+const {
+  CONFIG: { backendPort },
+  db,
+  cloudinary
+} = require("./conf");
 const bodyParser = require("body-parser");
-const upload = multer({ dest: 'tmp/' });
+const upload = multer({ dest: "tmp/" });
 
 app.use(bodyParser.json());
 app.use(
@@ -12,17 +17,58 @@ app.use(
     extended: true
   })
 );
-
 app.use(cors());
+app.use(passport.initialize());
+
+// Authentification
+app.use("/api/auth", require("./auth"));
 
 // USERS || GET & POST
 app.get("/api/users", (req, res) => {
   db.query(
-    "SELECT id, firstname, lastname, team_id, pseudo, sexe, address, avatar, email, age, country, city, creation_date, twitch, bio, mixer, youtube, LOL_pseudo, Fortnite_pseudo, CSGO_pseudo, OW_pseudo, HOTS_pseudo, SMITE_pseudo, APEX_pseudo, STARCRAFT2_pseudo, Hearstone_pseudo, KROSMAGA_pseudo, SSBU_pseudo, Tekken_pseudo, SF5_pseudo, ROCKETLEAGUE_pseudo, TFT_pseudo, PUBG_pseudo, R6S_pseudo, Paladins_pseudo from user",
+    "SELECT id, firstname, lastname, team_id, pseudo, gender, address, avatar, email, age, country, city, creation_date, twitch, bio, mixer, youtube, LOL_pseudo, Fortnite_pseudo, CSGO_pseudo, OW_pseudo, HOTS_pseudo, SMITE_pseudo, APEX_pseudo, STARCRAFT2_pseudo, Hearstone_pseudo, KROSMAGA_pseudo, SSBU_pseudo, Tekken_pseudo, SF5_pseudo, ROCKETLEAGUE_pseudo, TFT_pseudo, PUBG_pseudo, R6S_pseudo, Paladins_pseudo from user",
     (err, results) => {
       if (err) {
         res.status(500).send("Erreur lors de la récupération des données");
       } else {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+// USER || GET for the userPage
+app.get(
+  "/api/profile",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    db.query(
+      "SELECT user.avatar, user.id, user.pseudo, user.age, user.country, user.city, user.bio, user.team_id, user.role FROM user WHERE user.pseudo=?",
+      [req.user.pseudo],
+      (err, results) => {
+        if (err) {
+          res
+            .status(500)
+            .send("Erreur lors de la récupération des données de profile");
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  }
+);
+
+app.get("/api/user/posts/:id", (req, res) => {
+  db.query(
+    "SELECT post.id, user_id, user_id_team, game_id, message, date, image_url from post WHERE post.user_id = ?",
+    [req.params.id],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        console.log(req.query.id);
+
         res.status(200).json(results);
       }
     }
@@ -36,6 +82,7 @@ app.get("/api/posts/:limit", (req, res) => {
     [Number(req.params.limit)],
     (err, results) => {
       if (err) {
+        console.log(err);
         res.status(500).send(err);
       } else {
         res.status(200).json(results);
@@ -45,33 +92,24 @@ app.get("/api/posts/:limit", (req, res) => {
 });
 
 app.get("/api/gamelist/", (req, res) => {
-  db.query(
-    "SELECT * from game",
-    (err, results) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).json(results);
-      }
+  db.query("SELECT * from game", (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(results);
     }
-  );
+  });
 });
 
-app.post("/api/postimg", upload.single('file'), (req, res) => {
-
+app.post("/api/postimg", upload.single("file"), (req, res) => {
   const formData = req.file;
-
-
-
-  cloudinary.v2.uploader.upload(formData.path,
-    function (err, result) {
-      if (err) {
-        res.status(500).send("Erreur lors de la sauvegarde de l'image");
-      } else {
-
-        res.send(result);
-      }
-    });
+  cloudinary.v2.uploader.upload(formData.path, function(err, result) {
+    if (err) {
+      res.status(500).send("Erreur lors de la sauvegarde de l'image");
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.post("/api/posts", (req, res) => {
@@ -83,12 +121,10 @@ app.post("/api/posts", (req, res) => {
       res.sendStatus(201);
     }
   });
-})
-
-
-
+});
 
 app.listen(backendPort, err => {
+  console.log(backendPort + "Marche");
   if (err) {
     throw new Error("Something bad happened...");
   }
