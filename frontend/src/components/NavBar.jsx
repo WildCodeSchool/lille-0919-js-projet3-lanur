cd5913fd5bf0a112b392a657ed29faa00edb11a3
Jeremy VALLES <jer.valles@outlook.com>
2020-01-21 09:31:10 +0100

    added a new div for a better indentation of the css

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,17 +8,56 @@ function NavBar() {
 
   return (
     <div>
-      <div className="toggle_btn" onClick={() => setBurgerMenu(!burgerMenu)}>
-        <span></span>
-      </div>
-      {burgerMenu ? (
-        <div className="burgerMenuOn">
+      <div className="burgerMenu">
+        <div className="toggle_btn" onClick={() => setBurgerMenu(!burgerMenu)}>
+          <span></span>
+        </div>
+        {burgerMenu ? (
+          <div className="burgerMenuOn">
+            <ul>
+              <li onClick={() => setBurgerMenu(!burgerMenu)}>
+                <Link to="/newsfeed">
+                  <span className="FirstLetter">A</span>ccueil
+                </Link>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="FirstLetter">A</span>ctus
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="FirstLetter">T</span>eams
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="FirstLetter">A</span>genda
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span className="FirstLetter">R</span>ésultats
+                </a>
+              </li>
+              <li onClick={() => setBurgerMenu(!burgerMenu)}>
+                <Link to="/EditProfile">
+                  <span className="FirstLetter">P</span>rofil
+                </Link>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="burgerMenuOff"></div>
+        )}
+        <nav className="main-NavBar">
+          <h1>
+            <Link to="/newsfeed">
+              <span className="Lan">LAN'</span>U.R
+            </Link>
+          </h1>
+
           <ul>
-            <li onClick={() => setBurgerMenu(!burgerMenu)}>
-              <Link to="/newsfeed">
-                <span className="FirstLetter">A</span>ccueil
-              </Link>
-            </li>
             <li>
               <a href="#">
                 <span className="FirstLetter">A</span>ctus
@@ -29,82 +68,45 @@ function NavBar() {
                 <span className="FirstLetter">T</span>eams
               </a>
             </li>
-            <li>
+            <li onClick={() => setDisplayMenu(!displayMenu)}>
               <a href="#">
-                <span className="FirstLetter">A</span>genda
+                <span className="FirstLetter">É</span>vènements
+                {displayMenu ? (
+                  <img
+                    className="triangle"
+                    src="../images/Black_triangle_rotated.svg"
+                  ></img>
+                ) : (
+                  <img
+                    className="triangle"
+                    src="../images/Black_triangle.svg"
+                  ></img>
+                )}
               </a>
+              {displayMenu ? (
+                <div className="dropDownMenu">
+                  <li>
+                    <a href="#">
+                      <span className="FirstLetter">A</span>genda
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="FirstLetter">R</span>ésultats
+                    </a>
+                  </li>
+                </div>
+              ) : null}
             </li>
+
             <li>
-              <a href="#">
-                <span className="FirstLetter">R</span>ésultats
-              </a>
-            </li>
-            <li onClick={() => setBurgerMenu(!burgerMenu)}>
               <Link to="/EditProfile">
                 <span className="FirstLetter">P</span>rofil
               </Link>
             </li>
           </ul>
-        </div>
-      ) : (
-        <div className="burgerMenuOff"></div>
-      )}
-      <nav className="main-NavBar">
-        <h1>
-          <Link to="/newsfeed">
-            <span className="Lan">LAN'</span>U.R
-          </Link>
-        </h1>
-
-        <ul>
-          <li>
-            <a href="#">
-              <span className="FirstLetter">A</span>ctus
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span className="FirstLetter">T</span>eams
-            </a>
-          </li>
-          <li onClick={() => setDisplayMenu(!displayMenu)}>
-            <a href="#">
-              <span className="FirstLetter">É</span>vènements
-              {displayMenu ? (
-                <img
-                  className="triangle"
-                  src="../images/Black_triangle_rotated.svg"
-                ></img>
-              ) : (
-                <img
-                  className="triangle"
-                  src="../images/Black_triangle.svg"
-                ></img>
-              )}
-            </a>
-            {displayMenu ? (
-              <div className="dropDownMenu">
-                <li>
-                  <a href="#">
-                    <span className="FirstLetter">A</span>genda
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="FirstLetter">R</span>ésultats
-                  </a>
-                </li>
-              </div>
-            ) : null}
-          </li>
-
-          <li>
-            <Link to="/EditProfile">
-              <span className="FirstLetter">P</span>rofil
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 }
