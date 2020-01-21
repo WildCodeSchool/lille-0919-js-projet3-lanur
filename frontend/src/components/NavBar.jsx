5f4101fe31b9beb3287093694ec513636092a792
Adrien Imberdis <AdrienImberdis@gmail.com>
2020-01-21 17:51:58 +0100

    correction

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style/NavBar.scss";
-import { Link } from "react-router-dom";
 
 function NavBar() {
   const [displayMenu, setDisplayMenu] = useState(false);
--- frontend/src/store/reducer.js
+++ frontend/src/store/reducer.js
@@ -25,4 +25,3 @@ const reducer = (state = initialState, action) => {
 };
 
 export default reducer;
-
