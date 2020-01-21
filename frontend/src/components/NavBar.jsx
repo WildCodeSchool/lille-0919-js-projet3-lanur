b39c8bf81d6b0c770770ccf101a58a03077a7e52
Jeremy VALLES <jer.valles@outlook.com>
2020-01-21 17:17:24 +0100

    Added Burger menu in the navbar and it's Style

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { scaleDown as MenuBurger } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import "./style/NavBar.scss";
+import "./style/Burger.scss";
 
 function NavBar() {
   const [displayMenu, setDisplayMenu] = useState(false);
 
+  const burgerShowSettings = (e) => {
+    e.preventDefault();
+  };
   return (
     <nav className="main-NavBar">
       <h1>
@@ -60,6 +65,20 @@ function NavBar() {
           </Link>
         </li>
       </ul>
+      <MenuBurger width={"100%"} isOpen={true} id="MenuBurger">
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="about" className="menu-item" href="/about">
+          About
+        </a>
+        <a id="contact" className="menu-item" href="/contact">
+          Contact
+        </a>
+        <a onClick={burgerShowSettings} className="menu-item--small" href="">
+          Settings
+        </a>
+      </MenuBurger>
     </nav>
   );
 }
