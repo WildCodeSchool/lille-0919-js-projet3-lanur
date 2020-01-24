7cb0ce024c9d6f93b3a2d917ba151f3b78d19d39
nassilio <hubmattfac@fac.com>
2020-01-24 17:15:10 +0100

    created teams page to show list of teams

---
+++ frontend/src/components/Teams.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Image, CloudinaryContext } from "cloudinary-react";
+import { backend } from "../conf.js";
+import "./style/Teams.scss";
+
+function Team() {
+  const [teams, getTeams] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${backend}/api/teams/`)
+      .then(({ data }) => {
+        getTeams(data);
+      })
+      .catch(err => {});
+  }, []);
+  return (
+    <div className="teamPage">
+      <h1>Teams</h1>
+      {teams
+        ? teams.map(team => {
+            return (
+              <div className="teams">
+                <div className="img">
+                  <CloudinaryContext cloudName="teams">
+                    <Image publicId={team.image_team} alt="No image" />
+                  </CloudinaryContext>
+                </div>
+                <div className="teamInfo">
+                  <h2>{team.name}</h2>
+                  <p>{team.description}</p>
+                </div>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
+}
+
+export default Team;
