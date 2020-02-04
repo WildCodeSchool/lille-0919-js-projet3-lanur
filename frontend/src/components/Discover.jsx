42aae0d2c92e46df4500bf8fd0dec25fcd0f72c1
nassilio <55001600+nassilio@users.noreply.github.com>
2020-02-04 09:15:09 +0100

    replace

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -50,7 +50,7 @@ function Discover() {
           <Postcard
             key={post.message}
             message={post.message}
-            tags={post.tags ? "#" + post.tags.split(" ").join(" #") : null}
+            tags={post.tags ? "#" + post.tags.replace(" "," #") : null}
             date={post.date}
             image_url={post.image_url}
             game_id={post.game_id}
